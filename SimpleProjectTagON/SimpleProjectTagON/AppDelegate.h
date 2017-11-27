ca72ef0c7acee9aa18371eec8a2098ff00e45b17
onderozcan <onder@pixelblind.net>
2017-11-27 06:46:36 +0000

    TagON framework finalized

---
+++ SimpleProjectTagON/SimpleProjectTagON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleProjectTagON
+//
+//  Created by Önder ÖZCAN on 24/11/2017.
+//  Copyright © 2017 Önder ÖZCAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleProjectTagON/SimpleProjectTagON/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleProjectTagON
+//
+//  Created by Önder ÖZCAN on 24/11/2017.
+//  Copyright © 2017 Önder ÖZCAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

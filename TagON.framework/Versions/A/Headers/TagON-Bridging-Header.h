abac33fb12a32c4c284d8e9b1efa4f63536995b4
onderozcan <onder@pixelblind.net>
2018-12-28 17:52:26 +0000

    Swift Bridge added

---
+++ TagON.framework/Versions/A/Headers/TagON-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  TagON-Bridging-Header.h
+//  TagON Example
+//
+//  Created by Önder ÖZCAN on 28/12/2018.
+//  Copyright © 2018 Önder ÖZCAN. All rights reserved.
+//
+
+#import "TagAdapter.h"
+#import "TagFullPageImageView.h"
